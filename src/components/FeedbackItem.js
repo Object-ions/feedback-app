b0087c7542e8ad705ac0c9a89c6947f09e94eb6b
Object-ions <moshikoatia@gmail.com>
2024-02-14 00:29:15 -0800

    add new components

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,10 @@
+const FeedbackItem = () => {
+  return (
+    <div className="card">
+      <div className="num-display">10</div>
+      <div className="text-display">this is an exmaple</div>
+    </div>
+  );
+};
+
+export default FeedbackItem;
