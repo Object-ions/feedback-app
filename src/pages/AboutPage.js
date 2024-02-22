908b60c0f7ed4450b88c43f2224299045c627031
Object-ions <moshikoatia@gmail.com>
2024-02-21 16:51:34 -0800

    Add AboutPage component

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import Card from '../components/shared/Card';
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback to a product or a service </p>
+        <p>Version: 1.0.0</p>
+      </div>
+    </Card>
+  );
+};
+export default AboutPage;
