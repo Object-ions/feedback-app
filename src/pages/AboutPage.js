a6253e8a270c685da2178adae152f0576f9ce516
Object-ions <moshikoatia@gmail.com>
2024-02-21 17:18:08 -0800

    Add AboutIconLink component and link to AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Card from '../components/shared/Card';
 
 const AboutPage = () => {
@@ -7,6 +8,8 @@ const AboutPage = () => {
         <h1>About This Project</h1>
         <p>This is a React app to leave feedback to a product or a service </p>
         <p>Version: 1.0.0</p>
+
+        <Link to={'/'}>Back To Home</Link>
       </div>
     </Card>
   );
