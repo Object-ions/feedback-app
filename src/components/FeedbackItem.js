5eee49b64fc7a9a83c39b0e3100d865c246ef0db
Object-ions <moshikoatia@gmail.com>
2024-02-28 15:29:48 -0800

    Add edit feedback event

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,16 +1,19 @@
 import Card from './shared/Card';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
 const FeedbackItem = ({ item }) => {
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close" onClick={() => deleteFeedback(item.id)}>
         <FaTimes />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,20 +7,43 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This is feedback number 1',
       rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback number 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback number 3',
+      rating: 7,
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item: item,
+      edit: true,
+    });
   };
 
   return (
@@ -29,6 +52,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
